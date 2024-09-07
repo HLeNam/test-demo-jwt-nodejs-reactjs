d8771bab677c6bd20ef2e4bd3b92efae32bb9e1c
Yorickur <hoanglenam0905@gmail.com>
2024-09-07 13:23:49 +0700

    #8.4 Relationships

--- src/service/userService.js
+++ src/service/userService.js
@@ -4,6 +4,7 @@ import bcrypt from "bcryptjs";
 const salt = bcrypt.genSaltSync(10);
 
 import mysql from "mysql2/promise";
+import { where } from "sequelize/lib/sequelize";
 
 // Create the connection pool. The pool-specific settings are the defaults
 const connection = mysql.createPool({
@@ -41,58 +42,109 @@ const createNewUser = async (email, password, username) => {
 };
 
 const getUserList = async () => {
-    const [results, fields] = await connection.execute(
-        `
-            SELECT * FROM user
-        `
-    );
+    // Test
+    let t = await db.User.findOne({
+        where: { id: 1 },
+        attributes: ["id", "username", "email"],
+        include: { model: db.Group, attributes: ["name", "description"] },
+        raw: true,
+        nest: true,
+    });
 
-    let users = results || [];
+    let role = await db.Role.findAll({
+        include: { model: db.Group, where: { id: 1 } },
+        raw: true,
+        nest: true,
+    });
+
+    console.log(">>> check t:", t);
+    console.log(">>> check role:", role);
+
+    let users = [];
+
+    users = await db.User.findAll();
 
     return users;
+
+    // const [results, fields] = await connection.execute(
+    //     `
+    //         SELECT * FROM user
+    //     `
+    // );
+
+    // let users = results || [];
+
+    // return users;
 };
 
 const deleteUser = async (id) => {
-    const [results, fields] = await connection.execute(
-        `
-            DELETE FROM user
-            WHERE id = ?
-        `,
-        [id]
-    );
+    await db.User.destroy({
+        where: {
+            id: id,
+        },
+    });
 
-    return results;
+    // const [results, fields] = await connection.execute(
+    //     `
+    //         DELETE FROM user
+    //         WHERE id = ?
+    //     `,
+    //     [id]
+    // );
+
+    // return results;
 };
 
 const getUserById = async (id) => {
-    const [results, fields] = await connection.execute(
-        `
-            SELECT * FROM user
-            WHERE id = ?
-        `,
-        [id]
-    );
-
     let user = {};
 
-    if (results && results.length > 0) {
-        user = results[0];
-    }
+    user = await db.User.findOne({
+        where: {
+            id: id,
+        },
+    });
 
     return user;
+    // const [results, fields] = await connection.execute(
+    //     `
+    //         SELECT * FROM user
+    //         WHERE id = ?
+    //     `,
+    //     [id]
+    // );
+
+    // let user = {};
+
+    // if (results && results.length > 0) {
+    //     user = results[0];
+    // }
+
+    // return user;
 };
 
 const updateUserInfo = async (email, username, id) => {
-    const [results, fields] = await connection.execute(
-        `
-            UPDATE user
-            SET email = ?, username = ?
-            WHERE id = ?
-        `,
-        [email, username, id]
+    await db.User.update(
+        {
+            email,
+            username,
+        },
+        {
+            where: {
+                id,
+            },
+        }
     );
 
-    return results;
+    // const [results, fields] = await connection.execute(
+    //     `
+    //         UPDATE user
+    //         SET email = ?, username = ?
+    //         WHERE id = ?
+    //     `,
+    //     [email, username, id]
+    // );
+
+    // return results;
 };
 
 export { hashUserPassword, createNewUser, getUserList, deleteUser, getUserById, updateUserInfo };
